8a7b2d43429f72fc8a6a6e1231a5e29078cbea2f
Narmin <naghdaliyeva91@gmail.com>
2022-12-26 17:38:35 -0500

    test: qa123 add testing of login functionality

---
+++ src/narmin/TestingLoginFunctionality.java
@@ -0,0 +1,7 @@
+package narmin;
+
+public class TestingLoginFunctionality {
+    public static void main(String[] args) {
+        System.out.println("i m testing Login funct of my company");
+    }
+}
